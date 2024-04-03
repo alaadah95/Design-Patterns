bb6cb6b0a738066df594433c9a175cc664ef08ff
alaadah95 <35564348+alaadah95@users.noreply.github.com>
2024-04-03 11:36:59 +0200

    Decorator Design pattern

---
+++ Decorator design pattern/src/com/coffeepoweredcrew/decorator/Client.java
@@ -0,0 +1,14 @@
+package com.coffeepoweredcrew.decorator;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Message m = new TextMessage("The <FORCE> is strong with this one!");
+		System.out.println(m.getContent());
+		m = new HtmlEncodedMessage(m);
+		System.out.println(m.getContent());
+		m = new Base64EncodedMessage(m);
+		System.out.println(m.getContent());
+		
+	}
+}
---
+++ Decorator design pattern/src/com/coffeepoweredcrew/decorator/HtmlEncodedMessage.java
@@ -0,0 +1,20 @@
+package com.coffeepoweredcrew.decorator;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+//Decorator. Implements component interface
+public class HtmlEncodedMessage implements Message {
+
+	private Message message ;
+	
+	public HtmlEncodedMessage(Message message) {
+		this.message = message;
+	}
+
+
+	@Override
+	public String getContent() {
+		return StringEscapeUtils.escapeHtml4( message.getContent());
+	}
+
+}
